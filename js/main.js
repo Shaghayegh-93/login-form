764e98a7392426cd6e8f2168eb50a9cc78889a78
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-08-04 03:32:09 +0430

    all file added

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const loginForm=document.querySelector("form.login");
+const signupForm=document.querySelector("form.signup");
+const loginBtn=document.querySelector("label.login");
+const signupBtn=document.querySelector("label.signup");
+const signupLink=document.querySelector(".signup-link a");
+const signupText=document.querySelector(".title-text .signup");
+const loginText=document.querySelector(".title-text .login");
+
+
+
+signupBtn.addEventListener("click",()=>{
+    loginForm.style.marginLeft="-50%";
+    // signupText.style.marginLeft="-50%";
+    loginText.style.marginLeft="-50%"
+
+})
+loginBtn.addEventListener("click",()=>{
+    loginForm.style.marginLeft="0%";
+    loginText.style.marginLeft="0%"
+    // signupText.style.marginLeft="-50%";
+
+
+})
+
+signupLink.addEventListener("click",()=>{
+    signupBtn.click();
+})
+
+
+
+
+
